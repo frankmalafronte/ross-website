a1e8f6b8bb56abf9330d1011c06dc7d5408a680d
Frank <frankamalafronte@gmail.com>
2020-02-08 15:03:07 -0500

    adjusting marathon

--- src/pages/Marathon.js
+++ src/pages/Marathon.js
@@ -19,17 +19,12 @@ const Settlers = () => (
         <div className="column is-three-quarters is-size-5">
           <h1 className="title is-size-1 is-centered">Webscraping Marathons</h1>
           Webscraping and visualization of Nashville Rock N Roll Marathons, 2016-19
-
           <br />
           <br />
           <a className="text is-size-5" href="https://github.com/rossdingwall/webscraping_marathons">
           github.com/rossdingwall/webscraping_marathons
           </a>
           <br />
-          <a className="text is-size-5" href='https://github.com/Interactive-NYC/Settlers-Of-Catan'>
-            source code
-          </a>
-          
         </div>
       </div>
       <img src={SettlersImg}/>
